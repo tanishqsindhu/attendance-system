cfe6ef869e56bcf0396eb5198cee76d5db8ada65
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2025-02-25 19:43:51 +0530

    added redux files

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store/store";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ThemeProvider>
+		</Provider>
 	</StrictMode>
 );

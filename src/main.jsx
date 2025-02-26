3e302c574195b5a2931e4315c63eebadcb7efc58
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2025-02-26 15:13:44 +0530

    added clerk authentication

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,32 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { rootAuthLoader } from "@clerk/react-router/ssr.server";
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+export async function loader(args) {
+	return rootAuthLoader(args);
+}
+
+if (!PUBLISHABLE_KEY) {
+	throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Provider store={store}>
-			{/* <PersistGate loading={null} persistor={persistor}> */}
+		<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/users/login">
+			<Provider store={store}>
+				{/* <PersistGate loading={null} persistor={persistor}> */}
 				<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
 					<BrowserRouter>
 						<App />
 					</BrowserRouter>
 				</ThemeProvider>
-			{/* </PersistGate> */}
-		</Provider>
+				{/* </PersistGate> */}
+			</Provider>
+		</ClerkProvider>
 	</StrictMode>
 );
--- src/routes/users/users.components.jsx
+++ src/routes/users/users.components.jsx
@@ -6,13 +6,27 @@ import ProcessAttendance from "@/processAttendance";
 import LoginPage from "@/app/authentication/login/page";
 import RegisterPage from "@/app/authentication/register/page";
 
-const Users = () => {
-	return (
-		<Routes>
-			<Route path="login" element={<LoginPage />} />
-			<Route path="register" element={<RegisterPage />} />
-		</Routes>
-	);
-};
+// const Users = () => {
+// 	return (
+// 		<Routes>
+// 			<Route path="login" element={<LoginPage />} />
+// 			<Route path="register" element={<RegisterPage />} />
+// 		</Routes>
+// 	);
+// };
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
-export default Users;
+export default function Users() {
+	return (
+		<header>
+			<SignedOut>
+				<SignInButton>Login</SignInButton>
+			</SignedOut>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
+		</header>
+	);
+}
+
+// export default Users;
